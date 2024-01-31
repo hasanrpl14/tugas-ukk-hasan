096fe0b63c97cfbcde8a307a537dbb57df62efbe
hasanrpl14 <4bdisalam@gmail.com>
2024-01-31 23:30:42 +0700

    menambahkan fungsi tambah barang masuk datanya menambah di stok

--- fungsi/tambahBarangMasuk.php
+++ fungsi/tambahBarangMasuk.php
@@ -1,5 +1,4 @@
 <?php
-
 require '../koneksi.php';
 
 // menambahkan barang masuk
@@ -7,22 +6,20 @@ if(isset($_POST['barangmasuk'])){
     $idproduk = $_POST['ProdukID'];
     $qty = $_POST['JumlahProduk'];
 
-    $inserbarangmasuk = mysqli_query($c,"INSERT INTO masuk (ProdukID, JumlahProduk) VALUES('$idproduk','$qty')");
+    // Masukkan data barang masuk ke tabel 'masuk'
+    $insertbarangmasuk = mysqli_query($c,"INSERT INTO masuk (ProdukID, JumlahProduk) VALUES('$idproduk','$qty')");
 
-    // if (!$insertbarangmasuk) {
-    //     die('Error inserting masuk into produk: ' . mysqli_error($c));
-    // }
+    // Update stok barang di tabel 'produk'
+    $updateStok = mysqli_query($c, "UPDATE produk SET Stok = Stok + $qty WHERE ProdukID = '$idproduk'");
 
-    if($inserbarangmasuk){
+    if($insertbarangmasuk && $updateStok){
         header('location:../masuk.php');
     } else {
         echo '
-        <script>alert("gagal");
+        <script>alert("Gagal menambahkan barang masuk");
         window.location.href="../masuk.php"
         </script>
         ';
     }
 }
-
-
 ?>
